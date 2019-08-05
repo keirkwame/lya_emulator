e95d43366869c889325ff7df3c85ddfb2dc7ab59
Keir <keir.rogers@fysik.su.se>
2019-08-05 18:04:45 +0200

    Load the emulator(!)

--- plots/plot_flux_power.py
+++ plots/plot_flux_power.py
@@ -7,5 +7,6 @@ import lyaemu.coarse_grid as cg
 if __name__ == "__main__":
     emulator_base_directory = '/share/data2/keir/Simulations'
     emulator_instance = cg.nCDMEmulator(os.path.join(emulator_base_directory, 'nCDM_test2'))
+    emulator_instance.load()
 
     flux_powers = emulator_instance.get_flux_vectors(redshifts=emulator_instance.redshifts, pixel_resolution_km_s=1.)
