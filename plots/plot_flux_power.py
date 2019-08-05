f5dfc9f0e89e830598d464c51d8eb35e2333b887
Keir <keir.rogers@fysik.su.se>
2019-08-05 17:54:52 +0200

    Plot flux power spectra

---
+++ plots/plot_flux_power.py
@@ -0,0 +1,11 @@
+"""Plot 1D flux power spectra"""
+
+import os
+
+import lyaemu.coarse_grid as cg
+
+if __name__ == "__main__":
+    emulator_base_directory = '/share/data2/keir/Simulations'
+    emulator_instance = cg.nCDMEmulator(os.path.join(emulator_base_directory, 'nCDM_test2'))
+
+    flux_powers = emulator_instance.get_flux_vectors(redshifts=emulator_instance.redshifts, pixel_resolution_km_s=1.)
