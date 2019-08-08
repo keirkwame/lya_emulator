97a4dc46bc4037f3ebe860b8ff18e99944496c3b
Keir Rogers <keir.rogers@fysik.su.se>
2019-08-08 18:26:17 +0100

    Small fix to legend

--- plots/plot_tempdens.py
+++ plots/plot_tempdens.py
@@ -19,21 +19,21 @@ if __name__ == "__main__":
     redshifts = emulator_instance.redshifts
 
     savefile = os.path.join(emulator_base_directory, emulator_name, 'temperature_density.pdf')
-    figure, axes = plt.subplots(nrows=2, ncols=1, sharex='True')
+    figure, axes = plt.subplots(nrows=2, ncols=1)
 
     T0 = np.zeros((emulator_instance.get_parameters().shape[0], snapshot_numbers.shape[0]))
     gamma = np.zeros_like(T0)
 
     for i, input_parameters in enumerate(emulator_instance.get_parameters()):
+        print(input_parameters)
         simulation_directory = emulator_instance.get_outdir(input_parameters)
         for j, snapshot_number in enumerate(snapshot_numbers):
             T0[i, j], gamma[i, j] = td.fit_td_rel_plot(snapshot_number, simulation_directory, plot=False)
-        axes[0].plot(redshifts, T0[i], label=input_parameters)
-        axes[1].plot(redshifts, gamma[i], label=input_parameters)
-        print(input_parameters, T0[i], gamma[i])
+        axes[0].plot(redshifts, T0[i], label=i) #input_parameters)
+        axes[1].plot(redshifts, gamma[i], label=i) #input_parameters)
 
-    axes[0].legend(frameon=False)
-    axes[1].legend(frameon=False)
+    axes[0].legend(frameon=False, fontsize=7.)
+    axes[1].legend(frameon=False, fontsize=7.)
 
     axes[0].set_ylabel(r'$T_0 (z)$')
     axes[1].set_ylabel(r'$\gamma (z)$')
