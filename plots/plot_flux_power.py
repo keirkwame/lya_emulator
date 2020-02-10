6537090b90fed2df0646626fab8b174900ea439d
Keir Rogers <keir.rogers@fysik.su.se>
2020-02-10 17:46:34 +0000

    Minor changes

--- plots/plot_flux_power.py
+++ plots/plot_flux_power.py
@@ -60,9 +60,9 @@ if __name__ == "__main__":
                 k_cut = k_parallel[a][b, j] < 0.2
                 k_plot = k_parallel[a][b, j]
                 if interpolate_to_same_k:
-                    flux_powers_default = np.interp(k_plot[k_cut],
-                                                    k_parallel[default_emulator_index][default_simulation_index, j],
-                                                    flux_powers[default_emulator_index][default_simulation_index, j])
+                    flux_powers_default = 10 ** np.interp(np.log10(k_plot[k_cut]),
+                                                    np.log10(k_parallel[default_emulator_index][default_simulation_index, j]),
+                                                    np.log10(flux_powers[default_emulator_index][default_simulation_index, j]))
                 else:
                     flux_powers_default = flux_powers[default_emulator_index][default_simulation_index, j][k_cut]
                 axes[i][0].plot(np.log10(k_plot[k_cut]), np.log10(flux_powers[a][b, j] * k_plot / np.pi)[k_cut], label=r'$z = %.2f$'%redshift)
