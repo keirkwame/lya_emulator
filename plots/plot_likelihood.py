00fbd30a6aa061d2ca35529333c29054beee1496
Keir Rogers <keir.rogers@fysik.su.se>
2020-02-25 16:35:03 +0000

    Update plot

--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -239,7 +239,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if t0 != 1.0:
         sname = re.sub(r"\.","_", "tau0%.3g" % t0) + sname
 
-    filename_suffix = '_emu101_data_TDR_3000_hard_priors' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+    filename_suffix = '_emu101_data_TDR_15000_FDM_fixed_hot_prior' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
     datadir = os.path.join(sdir, "output")
@@ -248,7 +248,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if not os.path.exists(chainfile):
         #datadir = 'use_real_data'
         print('Beginning to sample likelihood at', str(datetime.now()))
-        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=3000,
+        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=15000, nsamples=15000,
                          prior_function=prior_function, while_loop=False, include_emulator_error=True,
                          n_threads=n_threads_mcmc)
         print('Done sampling likelihood at', str(datetime.now()))
@@ -307,16 +307,19 @@ if __name__ == "__main__":
     #test_simulation_parameters = np.concatenate((np.array([t0_test_value,] * 3), test_simulation_parameters[:-3], np.array([test_simulation_parameters[-2], 0.])))
 
     #Prior distribution
-    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'T_rei', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
-    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 16])] #7
-    #prior_means[6] = 5.55
-    #prior_means[7] = -1.8
-    prior_means[6] = 20000.
-    prior_means[7] = 10000.
-    prior_means[8] = 10000.
+    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'beta', 'gamma', 'T_rei', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
+    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16])] #7
+    prior_means[6] = 5.55
+    prior_means[7] = -1.8
+    prior_means[8] = 20000.
     prior_means[9] = 10000.
+    prior_means[10] = 10000.
+    prior_means[11] = 10000.
+    prior_means[12] = 1.6
+    prior_means[13] = 1.6
+    prior_means[14] = 1.6
     #prior_means = np.array([0.93, 2.3 * 1.e-9, 0.27])
-    prior_standard_deviations = np.array([0.005, 0.005, 0.005, 0.0057, 0.030 * 1.e-9, 0.001, 1000., 3000., 3000., 3000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
+    prior_standard_deviations = np.array([0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.001, 0.1, 0.1, 200., 3000., 3000., 3000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
     prior_function_args = (prior_parameter_names, prior_means, prior_standard_deviations)
     #prior_function_args = None
 
