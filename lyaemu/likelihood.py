e3a3e6fb0457b345fe6d285fecb5ab8ae5f197c8
keirkwame <keir.rogers@fysik.su.se>
2020-02-27 14:21:57 +0100

    Add numerical ULA model

--- lyaemu/likelihood.py
+++ lyaemu/likelihood.py
@@ -100,9 +100,38 @@ def ultra_light_axion_analytical_model(ultra_light_axion_parameters, nCDM_parame
     nCDM_parameters, nCDM_covariance = spo.curve_fit(transfer_function_nCDM, k_h_Mpc, T_k, p0=p0, bounds=bounds)
     return nCDM_parameters
 
-def ultra_light_axion_numerical_model():
-    """Model to map ultra-light axion parameters to nCDM parameters using a numerical Einstein-Boltzmann solver"""
-    pass
+def ultra_light_axion_alpha_model(log_mass, b, a, m, c):
+    """Model for alpha as a function of log ULA mass"""
+    return 10. ** ((b * (log_mass ** 3)) + (a * (log_mass ** 2)) + (m * log_mass) + c)
+
+def ultra_light_axion_beta_model(log_mass, a, m, c):
+    """Model for beta as a function of log ULA mass"""
+    return (a * (log_mass ** 2)) + (m * log_mass) + c
+
+def ultra_light_axion_gamma_model(log_mass, b, a, m, c):
+    """Model for gamma as a function of log ULA mass"""
+    return -1. * (10. ** ((b * (log_mass ** 3)) + (a * (log_mass ** 2)) + (m * log_mass) + c))
+
+def ultra_light_axion_numerical_model(ultra_light_axion_parameters, nCDM_parameter_limits, h=0.6686):
+    """Model to map ultra-light axion parameters to nCDM parameters using a fit to a numerical Einstein-Boltzmann
+    solver. Valid for -22 < log ULA mass [eV] < -18"""
+    alpha_model_parameters = np.array([5.54530089e-03, 3.31718138e-01, 6.16422310e+00, 3.31219369e+01])
+    beta_model_parameters = np.array([-0.02576259, -0.82153762, -0.45096863])
+    gamma_model_parameters = np.array([-1.29071567e-02, -7.52873377e-01, -1.47076333e+01, -9.60752318e+01])
+    h_planck = 0.6686
+
+    log_mass = ultra_light_axion_parameters[0]
+    alpha = ultra_light_axion_alpha_model(log_mass, *alpha_model_parameters)
+    beta = ultra_light_axion_beta_model(log_mass, *beta_model_parameters)
+    gamma = ultra_light_axion_gamma_model(log_mass, *gamma_model_parameters)
+    nCDM_parameters = np.array([alpha * h / h_planck, beta, gamma])
+
+    for i in range(3):
+        if nCDM_parameters[i] < nCDM_parameter_limits[i, 0]:
+            nCDM_parameters[i] = nCDM_parameter_limits[i, 0]
+        if nCDM_parameters[i] > nCDM_parameter_limits[i, 1]:
+            nCDM_parameters[i] = nCDM_parameter_limits[i, 1]
+    return nCDM_parameters
 
 
 class LikelihoodClass:
--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -205,13 +205,14 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
     #Measured parameter redshift model
     measured_parameter_names_z_model = None #np.array(['T_0', 'gamma']) #'T_0', 'u_0'
     measured_parameter_z_model_parameter_limits = np.array([[5000., 12000.], [-0.5, 0.5], [0.75, 1.75], [-0.5, 0.5]]) #A, S #[5000., 12000.], [-1., 1.]
-    like = likeh.LikelihoodClass(basedir=emudir, mean_flux=mean_flux_label,
+    like = likeh.UltraLightAxionLikelihoodClass(basedir=emudir, mean_flux=mean_flux_label,
                                  measured_parameter_names_z_model=measured_parameter_names_z_model, max_z=max_z,
                                  redshifts=redshifts, pixel_resolution_km_s=pixel_resolution_km_s,
                                  t0_training_value = t0_training_value, emulator_class=emulator_class,
                                  emulator_json_file=emulator_json_file, use_measured_parameters=use_measured_parameters,
                                  redshift_dependent_parameters=redshift_dependent_parameters, data_class=data_class,
-                                 measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits)
+                                 measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits,
+                                 dark_matter_model=likeh.ultra_light_axion_numerical_model)
 
     if prior_function == 'Gaussian':
         prior_function = lambda parameter_vector: like.log_gaussian_prior(parameter_vector,
@@ -239,7 +240,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if t0 != 1.0:
         sname = re.sub(r"\.","_", "tau0%.3g" % t0) + sname
 
-    filename_suffix = '_emu101_data_TDR_15000_FDM_fixed_hot_prior' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+    filename_suffix = '_emu50_data_TDR_300_ULA_fit' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
     datadir = os.path.join(sdir, "output")
@@ -248,7 +249,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if not os.path.exists(chainfile):
         #datadir = 'use_real_data'
         print('Beginning to sample likelihood at', str(datetime.now()))
-        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=15000, nsamples=15000,
+        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=300, nsamples=300,
                          prior_function=prior_function, while_loop=False, include_emulator_error=True,
                          n_threads=n_threads_mcmc)
         print('Done sampling likelihood at', str(datetime.now()))
@@ -307,9 +308,9 @@ if __name__ == "__main__":
     #test_simulation_parameters = np.concatenate((np.array([t0_test_value,] * 3), test_simulation_parameters[:-3], np.array([test_simulation_parameters[-2], 0.])))
 
     #Prior distribution
-    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'beta', 'gamma', 'T_rei', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
-    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16])] #7
-    prior_means[6] = 5.55
+    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
+    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 5, 11, 12, 13, 14, 15, 16])] #7
+    '''prior_means[6] = 5.55
     prior_means[7] = -1.8
     prior_means[8] = 20000.
     prior_means[9] = 10000.
@@ -317,9 +318,9 @@ if __name__ == "__main__":
     prior_means[11] = 10000.
     prior_means[12] = 1.6
     prior_means[13] = 1.6
-    prior_means[14] = 1.6
+    prior_means[14] = 1.6'''
     #prior_means = np.array([0.93, 2.3 * 1.e-9, 0.27])
-    prior_standard_deviations = np.array([0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.001, 0.1, 0.1, 200., 3000., 3000., 3000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
+    prior_standard_deviations = np.array([0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.001, 3000., 3000., 3000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
     prior_function_args = (prior_parameter_names, prior_means, prior_standard_deviations)
     #prior_function_args = None
 
