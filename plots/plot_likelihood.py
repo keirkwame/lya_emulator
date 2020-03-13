b2a2da3982ef2e7e50baf196543c8a4aa6c2e094
keirkwame <keir.rogers@fysik.su.se>
2020-03-13 13:14:48 +0100

    Clean-up plotting script a bit

--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -221,8 +221,9 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                                  measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits,
                                  dark_matter_model=likeh.ultra_light_axion_numerical_model,
                                  dark_matter_parameter_limits=np.array([[-22., -19.],]),
-                                 fix_parameters={'omega_m': 0.3209})
+                                 fix_parameters=None) #{'omega_m': 0.3209})
 
+    #Prior functions
     if prior_function == 'Gaussian':
         prior_function = lambda parameter_vector: like.log_gaussian_prior(parameter_vector,
                             parameter_names=prior_function_args[0], means=prior_function_args[1],
@@ -262,14 +263,13 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if t0 != 1.0:
         sname = re.sub(r"\.","_", "tau0%.3g" % t0) + sname
 
-    filename_suffix = '_emu50_data_TDR_u0_300_ULA_fit_convex_hull_omega_m_fixed_Planck_tau_T0_prior_no_jump_Tu0' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+    filename_suffix = '_emu101_data_TDR_u0_300_ULA_fit_convex_hull_omega_m_fix_Planck_tau_T0_prior_no_jump_Tu0' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
     datadir = os.path.join(sdir, "output")
     if true_parameter_values is None:
         true_parameter_values = get_simulation_parameters_s8(sdir, t0=t0)
     if not os.path.exists(chainfile):
-        #datadir = 'use_real_data'
         print('Beginning to sample likelihood at', str(datetime.now()))
         like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=300, nsamples=300,
                          prior_functions=prior_function, while_loop=False, include_emulator_error=True,
@@ -279,9 +279,8 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
         if like.use_dark_matter_model:
             true_parameter_values = np.delete(true_parameter_values, np.arange(6, 9))
             true_parameter_values = np.concatenate((true_parameter_values, np.array([-21.,])))
-
         #omega_m fixed
-        true_parameter_values = np.delete(true_parameter_values, 5, axis=0)
+        #true_parameter_values = np.delete(true_parameter_values, 5, axis=0)
 
         fp_savefile = os.path.join(savedir, 'flux_power_'+sname + ".pdf")
         make_plot_flux_power_spectra(like, true_parameter_values, datadir, savefile=fp_savefile, t0=t0,
@@ -334,8 +333,8 @@ if __name__ == "__main__":
     #test_simulation_parameters = np.concatenate((np.array([t0_test_value,] * 3), test_simulation_parameters[:-3], np.array([test_simulation_parameters[-2], 0.])))
 
     #Prior distribution
-    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2']) #, 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2']) #tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
-    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 9, 10, 11])] #, 12, 13, 14])] #0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14])] #, 15, 16])] #7
+    prior_parameter_names = np.array(['tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2']) #, 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2']) #tau0_0', 'tau0_1', 'tau0_2', 'ns', 'As', 'omega_m', 'T_0_z_5.0', 'T_0_z_4.6', 'T_0_z_4.2', 'gamma_z_5.0', 'gamma_z_4.6', 'gamma_z_4.2'])
+    prior_means = test_simulation_parameters[np.array([0, 1, 2, 3, 4, 5, 9, 10, 11])] #, 12, 13, 14])] #0, 1, 2, 3, 4, 5, 9, 10, 11, 12, 13, 14])] #, 15, 16])] #7
     '''prior_means[6] = 5.55
     prior_means[7] = -1.8
     prior_means[8] = 20000.
@@ -347,10 +346,9 @@ if __name__ == "__main__":
     prior_means[11] = 1.3'''
     #prior_means = np.array([0.93, 2.3 * 1.e-9, 0.27])
     #prior_means = np.array([0.3,])
-    prior_standard_deviations = np.array([0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 3000., 3000., 3000.]) #, 0.5, 0.5, 0.5]) #0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.001, 2000., 2000., 2000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
+    prior_standard_deviations = np.array([0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.00001, 3000., 3000., 3000.]) #, 0.5, 0.5, 0.5]) #0.05, 0.05, 0.05, 0.0057, 0.030 * 1.e-9, 0.001, 2000., 2000., 2000., 0.25, 0.25, 0.25]) #0.013]) #0.1, 0.1 * 1.e-9, 0.1])
     prior_function_args = (prior_parameter_names, prior_means, prior_standard_deviations)
     #prior_function_args = None
-
     #omega_m fixed
     #test_simulation_parameters = np.delete(test_simulation_parameters, 5, axis=0)
 
