f39954e084b9f72d482d44ab2a8ce9d45dc05898
Keir Rogers <keir.rogers@fysik.su.se>
2020-05-18 10:42:59 +0100

    Test changes

--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -276,7 +276,7 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                                  emulator_class=emulator_class, emulator_json_file=emulator_json_file,
                                  use_measured_parameters=use_measured_parameters,
                                  redshift_dependent_parameters=redshift_dependent_parameters,
-                                 flux_power_savefile='batch12_2_emulator_flux_vectors.hdf5',
+                                 flux_power_savefile='batch15_2_emulator_flux_vectors.hdf5',
                                  flux_power_parallel=True, flux_power_n_process=35, data_class=data_class,
                                  measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits,
                                  dark_matter_model=likeh.ultra_light_axion_numerical_model,
@@ -292,7 +292,7 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                             'standard_deviations': prior_function_args[2]}
 
     #Convex hull prior
-    parameter_names_convex_hull = [['T_0_z_5.0', 'gamma_z_5.0', 'u_0_z_5.0'], ['T_0_z_4.6', 'gamma_z_4.6', 'u_0_z_4.6'], ['T_0_z_4.2', 'gamma_z_4.2', 'u_0_z_4.2']]
+    parameter_names_convex_hull = [['T_0_z_5.0', 'u_0_z_5.0'], ['T_0_z_4.6', 'u_0_z_4.6'], ['T_0_z_4.2', 'u_0_z_4.2']]
     #prior_function_convex_hull = lambda parameter_vector: like.log_convex_hull_prior(parameter_vector,
     #                                                                        parameter_names=parameter_names_convex_hull)
     #convex_hulls = [None] * 3
@@ -332,7 +332,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if t0 != 1.0:
         sname = re.sub(r"\.","_", "tau0%.3g" % t0) + sname
 
-    filename_suffix = '_batch12_data_diag_emu_TDR_u0_15000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tgu0CH_0' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+    filename_suffix = '_batch15_data_diag_emu_TDR_u0_15000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tu0CH_0_T012_g08_u012_18' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
     datadir = os.path.join(sdir, "output")
@@ -343,11 +343,11 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
         #pool_instance = mg.Pool(2) #None #MyPool()
 
         #Change prior
-        like.param_limits[5, 1] = 10000.
-        like.param_limits[np.array([6, 7]), 1] = 13000.
-        like.param_limits[np.array([8, 9, 10]), 0] = 1.
-        like.param_limits[11, 1] = 10.
-        like.param_limits[np.array([12, 13]), 1] = 15.
+        like.param_limits[5, 1] = 12000.
+        #like.param_limits[np.array([6, 7]), 1] = 15000.
+        like.param_limits[np.array([8, 9, 10]), 0] = 0.8
+        like.param_limits[11, 1] = 12.
+        like.param_limits[np.array([12, 13]), 1] = 18.
 
         like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=15000,
                          while_loop=False, include_emulator_error=True, pool=None)
