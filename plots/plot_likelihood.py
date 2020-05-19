ff50a732c240517393cfecfd4dc1ce8b4972fa7a
Keir Rogers <keir.rogers@fysik.su.se>
2020-05-19 12:13:42 +0100

    Test changes

--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -276,8 +276,8 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                                  emulator_class=emulator_class, emulator_json_file=emulator_json_file,
                                  use_measured_parameters=use_measured_parameters,
                                  redshift_dependent_parameters=redshift_dependent_parameters,
-                                 flux_power_savefile='batch16_2_emulator_flux_vectors.hdf5',
-                                 flux_power_parallel=True, flux_power_n_process=35, data_class=data_class,
+                                 flux_power_savefile='batch15_1_emulator_flux_vectors.hdf5',
+                                 flux_power_parallel=True, flux_power_n_process=18, data_class=data_class,
                                  measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits,
                                  dark_matter_model=likeh.ultra_light_axion_numerical_model,
                                  dark_matter_parameter_limits=np.array([[-22., -19.],]),
@@ -332,7 +332,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if t0 != 1.0:
         sname = re.sub(r"\.","_", "tau0%.3g" % t0) + sname
 
-    filename_suffix = '_batch16_data_diag_emu_TDR_u0_3000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tu0CH_0_T012_g08_u012_18' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+    filename_suffix = '_batch15_1_data_diag_emu_TDR_u0_15000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tu0CH_0_T012_g08_u012_18' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
     datadir = os.path.join(sdir, "output")
@@ -349,7 +349,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
         like.param_limits[11, 1] = 12.
         like.param_limits[np.array([12, 13]), 1] = 18.
 
-        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=3000,
+        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=15000,
                          while_loop=False, include_emulator_error=True, pool=None)
         #likeh.do_posterior_sampling_parallel(like, chainfile, datadir='use_real_data', nwalkers=150, burnin=300,
         #                                     nsamples=300, while_loop=False, include_emulator_error=True)
