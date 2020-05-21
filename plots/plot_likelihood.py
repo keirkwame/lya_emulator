3d571cd227ae687591e2d293db5f11eccf1591b3
Keir Rogers <keir.rogers@fysik.su.se>
2020-05-21 18:22:35 +0100

    Test changes

--- plots/plot_likelihood.py
+++ plots/plot_likelihood.py
@@ -277,8 +277,8 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                                  emulator_class=emulator_class, emulator_json_file=emulator_json_file,
                                  use_measured_parameters=use_measured_parameters,
                                  redshift_dependent_parameters=redshift_dependent_parameters,
-                                 flux_power_savefile='batch16_2_emulator_flux_vectors.hdf5',
-                                 flux_power_parallel=True, flux_power_n_process=18, data_class=data_class,
+                                 flux_power_savefile='batch17_1_emulator_flux_vectors.hdf5',
+                                 flux_power_parallel=True, flux_power_n_process=35, data_class=data_class,
                                  measured_parameter_z_model_parameter_limits=measured_parameter_z_model_parameter_limits,
                                  dark_matter_model=likeh.ultra_light_axion_numerical_model,
                                  dark_matter_parameter_limits=np.array([[-22., -19.],]),
@@ -322,7 +322,7 @@ def run_likelihood_test(testdir, emudir, savedir=None, prior_function='uniform',
                                t0=t0_training_value, true_parameter_values=test_simulation_parameters,
                                data_class=data_class, pixel_resolution_km_s=pixel_resolution_km_s,
                                mean_flux_label=mean_flux_label, plot_parameter_indices=plot_parameter_indices,
-                               n_threads_mcmc=n_threads_mcmc)
+                               n_threads_mcmc=n_threads_mcmc, leave_out_validation=leave_out_validation)
     return like
 
 def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=True, t0=1., true_parameter_values=None,
@@ -337,8 +337,8 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
     if leave_out_validation is None:
         validation_suffix = ''
     else:
-        validation_suffix = '_' + leave_out_validation[0]
-    filename_suffix = '_batch16_2_data_diag_emu_TDR_u0_3000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tu0CH_0_T012_g08_u012_18' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
+        validation_suffix = '_' + str(leave_out_validation[0])
+    filename_suffix = '_batch17_1_data_diag_emu_TDR_u0_15000_ULA_fit_convex_hull_omega_m_fixed_tau_Planck_T0_tighter_prior_no_jump_Tu0_Tu0CH_0_T012_g08_u012_18' #'_mf_free_prior_measured_TDR_gamma_power_law_T0_prior_3000'
     filename_suffix += validation_suffix
     chainfile = os.path.join(savedir, 'chain_' + sname + filename_suffix + '.txt')
     sname = re.sub(r"\.", "_", sname)
@@ -356,7 +356,7 @@ def single_likelihood_plot(sdir, like, savedir, prior_function='uniform', plot=T
         like.param_limits[11, 1] = 12.
         like.param_limits[np.array([12, 13]), 1] = 18.
 
-        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=3000,
+        like.do_sampling(chainfile, datadir='use_real_data', nwalkers=150, burnin=3000, nsamples=15000,
                          while_loop=False, include_emulator_error=True, pool=None)
         #likeh.do_posterior_sampling_parallel(like, chainfile, datadir='use_real_data', nwalkers=150, burnin=300,
         #                                     nsamples=300, while_loop=False, include_emulator_error=True)
@@ -387,7 +387,7 @@ if __name__ == "__main__":
     test_name = sys.argv[3] #'nCDM_test_thermal2'
     parameters_json = sys.argv[4] #'emulator_params_measured_TDR.json'
     use_measured_parameters = (sys.argv[5].lower() == 'true')
-    leave_out_validation = np.array([int(sys.argv[6]),])
+    leave_out_validation = None #np.array([int(sys.argv[6]),])
     redshift_dependent_parameters = True #(sys.argv[6].lower() == 'true')
 
     plotdir = 'Plots' #'plots/simulations2'
