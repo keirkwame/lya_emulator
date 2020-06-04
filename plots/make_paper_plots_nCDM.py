bc8616e240083f4cec27b3d672cdb5ade4fec060
*Keir <*keir.rogers@fysik.su.se*>
2020-06-04 16:58:25 +0100

    Make cross-validation plots

--- plots/make_paper_plots_nCDM.py
+++ plots/make_paper_plots_nCDM.py
@@ -1,10 +1,14 @@
+import copy as cp
 import numpy as np
+import numpy.random as npr
 import numpy.testing as npt
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sb
 
 import lyaemu.distinct_colours_py3 as lyc
-import lyaemu.coarse_grid as lyc
-import lyaemu.mean_flux as lym
+#import lyaemu.coarse_grid as lyc
+#import lyaemu.mean_flux as lym
 #from lyaemu.likelihood import transfer_function_nCDM
 
 #Define global variables
@@ -124,9 +128,154 @@ def make_error_distribution():
 
     return k, emu_instance_full.redshifts, training_parameters, training_flux_powers, np.array(GP_mean), np.array(GP_std), emu_instance_full._get_k_max_emulated_h_Mpc()
 
-def violinplot_error_distribution():
+def violinplot_error_distribution(distribution='validation'):
     """Make a violin-plot of the emulator error distribution for leave-one-out cross-validation."""
+    n_sims = 93
+    n_LH = 50
+    n_BO = n_sims - n_LH
+    n_mf = 2
+    n_k_cut = 45
+    n_k_data = 16
+    k_bins = np.concatenate((np.repeat(1, 15), np.repeat(2, 15), np.repeat(3, 15)))
 
+    #Load data
+    validation_data = np.load('/Users/keir/Software/lya_emulator/plots/cross_validation.npz')
+    k = validation_data['k']
+    redshifts = validation_data['z']
+    p = validation_data['p']
+    f = validation_data['f']
+    m = validation_data['m']
+    s = validation_data['s']
+    k_max = validation_data['k_max']
+    f_cut = np.concatenate((f[:, :n_k_cut], f[:, k.size:k.size+n_k_cut], f[:, 2*k.size:(2*k.size)+n_k_cut]), axis=1)
+
+    #Load Boera+ data
+    s_data = np.zeros(n_k_data * redshifts.size)
+    for i, z in enumerate(redshifts):
+        data_all = np.genfromtxt('/Users/keir/Software/lya_emulator/lyaemu/data/Boera_HIRES_UVES_flux_power/flux_power_z_%.1f.dat'%z,
+                                    skip_header=5, skip_footer=1)
+        s_data[(i * n_k_data): ((i + 1) * n_k_data)] = data_all[:, 3]
+        if i == 0:
+            k_data = 10. ** data_all[:, 0]
+
+    k_expand = np.tile(k[:n_k_cut], (n_sims * n_mf, redshifts.size))
+    vel_fac = (1. + redshifts[np.newaxis, :]) / (100. *
+                np.sqrt((p[:(n_sims*n_mf), 3][:, np.newaxis] *
+                ((1. + redshifts[np.newaxis, :]) ** 3.)) + (1. - p[:(n_sims*n_mf), 3][:, np.newaxis])))
+    vel_fac = np.repeat(vel_fac, n_k_cut, axis=1)
+    k_kms = k_expand * vel_fac
+
+    k_nearest_data = np.ones_like(k_kms) * k_data[0]
+    s_data_expand = np.zeros_like(k_kms)
+    #k_index = np.zeros_like(k_kms)
+    for i, k_datum in enumerate(k_data):
+        for j, z in enumerate(redshifts):
+            slice_array = np.absolute(k_kms[:, (j * n_k_cut): ((j + 1) * n_k_cut)] - k_datum) <=\
+                          np.absolute(k_kms - k_nearest_data)[:, (j * n_k_cut): ((j + 1) * n_k_cut)]
+            k_nearest_data[:, (j * n_k_cut): ((j + 1) * n_k_cut)][slice_array] = k_datum
+            s_data_expand[:, (j * n_k_cut): ((j + 1) * n_k_cut)][slice_array] = s_data[(j * n_k_data) + i]
+
+    #LH-BO cut
+    if distribution == 'validation':
+        errors = (m - f_cut) / s
+        kernel_bw = 0.5
+        colours = lyc.get_distinct(3)
+        ylim = [-4.9, 4.9]
+        ylabel = r'$(\mathrm{Mean - Truth})\,/\,\sigma$'
+        text_height = 0.85
+        legend_loc = 'upper left'
+        save_file = 'validation.pdf'
+    elif distribution == 'data':
+        errors = np.log10(s[:(n_sims * n_mf)] / s_data_expand) #np.log10(
+        errors_real = np.log10(np.absolute(m - f_cut)[:(n_sims * n_mf)] / s_data_expand)
+        kernel_bw = 'scott' #0.01
+        colours = lyc.get_distinct(4)
+        ylim = [-5., 2.5]
+        ylabel = r'$\mathrm{log}\,(\sigma_\mathrm{Theory}\,/\,\sigma_\mathrm{Data})$'
+        text_height = 0.1
+        legend_loc = 'lower center'
+        save_file = 'data_error.pdf'
+    LH_cut = np.sort([np.arange(i, i+(n_LH + n_BO), 1) for i in range(0, n_sims*n_mf, n_sims)], axis=None)
+    BO_cut = np.sort([np.arange(i+n_LH, i+n_sims, 1) for i in range(0, n_sims*n_mf, n_sims)], axis=None)
+    errors_LH = errors[LH_cut]
+    errors_BO = errors[BO_cut]
+    errors_list = [errors_BO, errors_LH]
+    if distribution == 'data':
+        errors_list_real = [errors_real[BO_cut], errors_real[LH_cut]]
+
+    #k bins
+    #k_bins_input = k[:n_k_cut]
+    k_bins_input = cp.deepcopy(k_bins)
+    k_bin_LH = np.tile(k_bins_input, ((n_LH + n_BO) * n_mf, redshifts.size))
+    k_bin_BO = np.tile(k_bins_input, (n_BO * n_mf, redshifts.size))
+    k_bin_list = [k_bin_BO, k_bin_LH]
+
+    #BO
+    fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(6.4*2., 6.4*2.5)) #6.4*1.))
+    data_frames = [None] * redshifts.size * len(errors_list)
+    for i, z in enumerate(redshifts):
+        #z cut
+        z_cut = np.arange(i*n_k_cut, (i+1)*n_k_cut, 1)
+        for j in range(len(errors_list)):
+            idx = (j * redshifts.size) + i
+            if distribution == 'validation':
+                k_bin_df = np.tile(np.ravel(k_bin_list[j][:, z_cut]), 21)
+                errors_df = np.concatenate((np.ravel(errors_list[j][:, z_cut]), npr.normal(size=errors_list[j][:, z_cut].size * 20)))
+                if j == 0:
+                    samples_label = r'Validation test [optimisation simulations]'
+                else:
+                    samples_label = r'Validation test [all simulations]'
+                samples_label_real = 'Unit Gaussian model'
+                if j == 0:
+                    violin_colours = {samples_label: colours[0], samples_label_real: colours[2]}
+                else:
+                    violin_colours = {samples_label: colours[1], samples_label_real: colours[2]}
+                split_cut_df = ([samples_label,] * errors_list[j][:, z_cut].size) + ([samples_label_real,] * errors_list[j][:, z_cut].size * 20)
+            elif distribution == 'data':
+                k_bin_df = np.tile(np.ravel(k_bin_list[j][:, z_cut]), 2)
+                errors_df = np.concatenate(
+                    (np.ravel(errors_list[j][:, z_cut]), np.ravel(errors_list_real[j][:, z_cut])))
+                if j == 0:
+                    samples_label = r'Emulator / Data [optimisation simulations]'
+                    samples_label_real = r'$|\mathrm{Mean - Truth}|$ / Data [optimisation simulations]'
+                    violin_colours = {samples_label: colours[0], samples_label_real: colours[1]}
+                else:
+                    samples_label = r'Emulator / Data [all simulations]'
+                    samples_label_real = r'$|\mathrm{Mean - Truth}|$ / Data [all simulations]'
+                    violin_colours = {samples_label: colours[2], samples_label_real: colours[3]}
+                split_cut_df = ([samples_label,] * errors_list[j][:, z_cut].size) + ([samples_label_real,] * errors_list[j][:, z_cut].size)
+            print(i, j, k_bin_df.shape, errors_df.shape, len(split_cut_df))
+
+            data_frames[idx] = pd.DataFrame({'kbin': k_bin_df, 'ErrorSigmas': errors_df, 'Distribution': split_cut_df})
+            sb.violinplot(data_frames[idx].kbin, data_frames[idx].ErrorSigmas, data_frames[idx].Distribution,
+                          ax=axes[idx], scale='width', bw=kernel_bw, inner=None, split=True, palette=violin_colours, cut=0.,
+                          linewidth=2.5, saturation=1.)
+            # #cut=0
+
+            axes[idx].set(ylim=ylim)
+            axes[idx].axhline(y=0., color='black', ls=':', lw=2.5)
+            axes[idx].axvline(x=0., color='black', ls='-', lw=2.5)
+            axes[idx].axvline(x=1., color='black', ls='-', lw=2.5)
+            axes[idx].axvline(x=2., color='black', ls='-', lw=2.5)
+            axes[idx].text(0.9, text_height, r'$z = %.1f$'%redshifts[i], transform=axes[idx].transAxes) #, fontsize=16.)
+            axes[idx].get_legend().remove()
+            axes[idx].set(ylabel=ylabel)
+            #if idx < 5:
+            #axes[idx].xaxis.set_ticklabels([])
+            if (idx == 0) or (idx == 3):
+                axes[idx].legend(loc=legend_loc, frameon=True, facecolor='white', fancybox=False, shadow=False,
+                                 framealpha=1., edgecolor='white', fontsize=15.)
+            if idx < 5:
+                axes[idx].xaxis.set_visible(False)
+            else:
+                axes[idx].set(xlabel=r'$k [h\,\mathrm{Mpc}^{-1}]$ bin')
+            if distribution == 'data':
+                axes[idx].axhline(y=1., color='black', ls=':', lw=2.5)
+                axes[idx].axhline(y=-1., color='black', ls=':', lw=2.5)
+
+    fig.subplots_adjust(top=0.99, bottom=0.05, right=0.95, hspace=0.1)
+    plt.savefig('/Users/keir/Documents/emulator_paper_axions/' + save_file)
+    return k, z, p, f, m, s, k_data, s_data, k_max, data_frames
 
 if __name__ == "__main__":
     plt.rc('text', usetex=True)
@@ -139,5 +288,5 @@ if __name__ == "__main__":
     plt.rc('ytick.minor', width=1.5)
 
     #plot_transfer_function()
-    k, z, p, f, m, s, k_max = make_error_distribution()
-
+    #k, z, p, f, m, s, k_max = make_error_distribution()
+    k, z, p, f, m, s, k_data, s_data, k_max, data_frames = violinplot_error_distribution(distribution='data')
